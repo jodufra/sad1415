3709ee303d6087250a1dca865d59202909ffc354
jodufra <joeld.fran@hotmail.com>
2015-06-02 02:00:25 +0100

    .

--- Projecto_ETL^2/scripts_transform/CREATE_data_tables.sql
+++ Projecto_ETL^2/scripts_transform/CREATE_data_tables.sql
@@ -1,81 +1,105 @@
+
 CREATE TABLE t_clean_avaliacoes(
-	cd_lectivo	VARCHAR2(7), 
-	cd_duracao	VARCHAR2(2), 
-	cd_aluno	NUMBER(38), 
-	cd_curso_aluno	NUMBER(38), 
-	cd_discip	NUMBER(38), 
-	cd_plano	NUMBER(38), 
-	cd_epoca_aval	NUMBER(38), 
-	ds_epoca_aval 	VARCHAR2(50), 
-	nr_avalia	NUMBER(38) 
+	cd_lectivo VARCHAR2(7), 
+	cd_duracao VARCHAR2(2), 
+	cd_aluno NUMBER(38), 
+	cd_curso_aluno NUMBER(38), 
+	cd_discip NUMBER(38), 
+	cd_plano NUMBER(38), 
+	cd_epoca_aval NUMBER(38), 
+	ds_epoca_aval VARCHAR2(50), 
+	nr_avalia NUMBER(38) 
 );
 
 CREATE TABLE t_clean_cursos(
-	cd_curso	NUMBER(38), 
-	cd_oficial	VARCHAR2(4), 
-	nm_curso	VARCHAR2(240), 
-	nm_cur_abr	VARCHAR2(40), 
-	cd_instituic	NUMBER(38), 
-	cd_regime	VARCHAR2(500), 
-	ds_area_estudo	VARCHAR2(100), 
-	ds_grau	VARCHAR2(100), 
-	cd_activo	VARCHAR2(1), 
-	cd_bolonha	VARCHAR2(1) 
+	curso_natural_key NUMBER(38), 
+	curso_oficial_key VARCHAR2(4), 
+	curso_nome VARCHAR2(240), 
+	nm_cur_abr VARCHAR2(40), 
+	curso_nome_abv NUMBER(38), 
+	curso_regime VARCHAR2(500), 
+	ds_area_estudo VARCHAR2(100), 
+	curso_grau VARCHAR2(100), 
+	curso_activo VARCHAR2(3), 
+	curso_bolonha VARCHAR2(3),
+	curso_instituicao_key NUMBER(*,0),
+	curso_instituicao_nome VARCHAR2(100),
+	curso_instituicao_nome_abv VARCHAR2(30),
+
 );
 
 CREATE TABLE t_clean_estudantes(
-	cd_curso  NUMBER(38), 
-	cd_aluno  NUMBER(38)
+	estudante_key NUMBER(38), 
+	curso_key NUMBER(38),
+	estudante_natural_key NUMBER(38)
 );
 
 CREATE TABLE t_clean_inscricoes(
-	cd_lectivo  VARCHAR2(7),
-	cd_curso_aluno  NUMBER(38),
-	cd_plano  NUMBER(38),
-	cd_ramo  NUMBER(38),
-	cd_discip  NUMBER(38),
-	cd_aluno  NUMBER(38),
-	dt_inscri	DATE,
-	cd_tipo_insc  NUMBER(38),
-	ds_tipo_insc	VARCHAR2(50),
-	ects	FLOAT(126),
-	cd_epoca_aval	NUMBER,
-	ds_epoca_aval	VARCHAR2(50)
+	cd_lectivo VARCHAR2(7),
+	cd_curso_aluno NUMBER(38),
+	cd_plano NUMBER(38),
+	cd_ramo NUMBER(38),
+	cd_discip NUMBER(38),
+	cd_aluno NUMBER(38),
+	dt_inscri DATE,
+	cd_tipo_insc NUMBER(38),
+	ds_tipo_insc VARCHAR2(50),
+	ects FLOAT(126),
+	cd_epoca_aval NUMBER,
+	ds_epoca_aval VARCHAR2(50)
 );
 
 CREATE TABLE t_clean_tipos_inscricao(
-	cd_tipo_insc  NUMBER(38),
-	ds_tipo_insc	VARCHAR2(50)
+	tipo_insc_natural_key NUMBER(38),
+	tipo_insc_descricao VARCHAR2(50)
 );
 
 CREATE TABLE t_clean_planos(
-	cd_curso  NUMBER(38),
-	cd_plano  NUMBER(38),
-	nm_plano	VARCHAR2(280),
-	cd_activo	VARCHAR2(1),
-	nr_ects_curso	NUMBER(38),
-	nr_duracao_curso	NUMBER(3)
+	cd_curso NUMBER(38),
+	cd_plano NUMBER(38),
+	nm_plano VARCHAR2(280),
+	cd_activo VARCHAR2(1),
+	nr_ects_curso NUMBER(38),
+	nr_duracao_curso NUMBER(3)
 );
 
 CREATE TABLE t_clean_ramos(
-	cd_curso  NUMBER(38), 
-	cd_plano  NUMBER(38), 
-	cd_ramo  NUMBER(38), 
+	cd_curso NUMBER(38), 
+	cd_plano NUMBER(38), 
+	cd_ramo NUMBER(38), 
 	nm_ramo	VARCHAR2(280) 
 );
 
 CREATE TABLE t_clean_unidades_curriculares(
-	cd_plano  NUMBER(38),
-	cd_discip  NUMBER(38),
-	ds_discip	VARCHAR2(200),
-	ds_abreviatura	VARCHAR2(15),
-	cd_duracao	VARCHAR2(2),
-	cd_ramo  NUMBER(38),
-	cd_curso  NUMBER(38)
+	uc_natural_key NUMBER(38),
+	curso_key NUMBER(38),
+	uc_nome VARCHAR2(200),
+	uc_nome_abv VARCHAR2(10),
+	uc_duracao VARCHAR2(2),
+	uc_area_cientifica VARCHAR2(150),
+	uc_area_cientifica_abv VARCHAR2(10),
+	uc_departamento_abv VARCHAR2(10),
+	ramo_key NUMBER(38),
+	uc_ramo NUMBER(38),
+	plano_key NUMBER(38),
+	uc_plano  VARCHAR2(280),
+	uc_plano_activo VARCHAR2(3),
+	uc_plano_ano NUMBER(2),
+	uc_plano_semestre NUMBER(2)
+
 );
 
 CREATE TABLE t_clean_unidades_organicas(
-	cd_instituic  NUMBER(38),
-	ds_instituic  VARCHAR2(100),
+	cd_instituic NUMBER(38),
+	ds_instituic VARCHAR2(100),
 	ds_inst_abr	VARCHAR2(30)
+);
+
+CREATE TABLE t_clean_epoca_avaliacao(
+	epoca_natural_key NUMBER(38),
+	epoca_descricao VARCHAR2(200),
+	epoca_semestre_anoletivo VARCHAR2(100),
+	epoca_semestre VARCHAR2(100),
+	epoca_anoletivo VARCHAR2(100)
+
 );
